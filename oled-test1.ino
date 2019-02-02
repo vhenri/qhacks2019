6991aaa193a7326494e7bcce7b6679eda7162d21
Unknown <anthony.crudo@queensu.ca>
2019-02-02 02:02:07 -0500

    Adding working OLED Screen Test
    Simple screen showing time.

---
+++ oled-test1.ino
@@ -0,0 +1,38 @@
+// This #include statement was automatically added by the Particle IDE.
+#include <Adafruit_SSD1306.h>
+
+/* ============== MAIN =====================*/
+
+ //Use I2C with OLED RESET pin on D4
+ #define OLED_RESET D4
+ Adafruit_SSD1306 oled(OLED_RESET);
+
+
+ unsigned long previousMillis;
+ unsigned long interval = 30000;
+
+ void setup() {
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // init done     
+     
+  //oled.display(); // show splashscreen
+
+  Time.zone(-4);
+  
+   
+}
+
+void loop() {
+   oled.clearDisplay();
+  delay(200);
+  oled.setTextSize(2);
+  oled.setTextColor(WHITE);
+  oled.setCursor(0,0);
+  oled.print(Time.hourFormat12()); oled.print(":"); oled.print(Time.minute()); oled.print(":"); oled.print(Time.second());
+  oled.setTextColor(BLACK, WHITE); // 'inverted' text
+  
+  oled.display();
+  
+  delay(800);
+}
